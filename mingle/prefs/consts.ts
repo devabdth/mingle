4ebb56c35af9c95e581325a4913aaed0f451f3df
devabdth <a.tharwat@cubersio.com>
2024-04-25 00:19:36 +0200

    Consts Init

---
+++ mingle/prefs/consts.ts
@@ -0,0 +1,22 @@
+ export const Tabs= [
+     {
+         label: "Home",
+         href: "/home/",
+     },
+     {
+         label: "Collections",
+         href: "/collections/",
+     },
+     {
+         label: "Artboards",
+         href: "/artboards/",
+     },
+     {
+         label: "Mirrors",
+         href: "/mirrors/",
+     },
+     {
+         label: "About Us",
+         href: "/about/",
+     },
+ ];
