bd4e405d42865c419b5786ee8d9fa1d01edc775c
devabdth <a.tharwat@cubersio.com>
2024-04-25 10:08:05 +0200

    Hero Section Init

---
+++ mingle/components/home/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Image from "next/image";
+import { DefaultButton } from "@/components/globals";
+
+export default () => {
+    return (
+        <section
+         className="w-full h-[90vh] flex flex-col border-2 border-black"
+        ></section>
+    );
+}
